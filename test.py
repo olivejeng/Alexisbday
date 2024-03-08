2785ab906c4258e2509007a8e8656919721fc0d0
olivejeng <emailolivej@gmail.com>
2024-03-08 14:02:28 -0500

    python practice

---
+++ test.py
@@ -0,0 +1,6 @@
+
+name = input("Whats your name? ")
+food = input("Whats your favorite food? ")
+age = input("Whats your age? ")
+random_word = input("Give me a random word: ")
+print(f"My name  is {random_word}, i love the food {age} and im {food} years old!Another fun fact is that my favorite flowers are {name} tulips!")
